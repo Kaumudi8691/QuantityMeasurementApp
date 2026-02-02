1b2d4a38f0c152f8be6f915542ea7b6b8b833d84
Kaumudi8691 <kaumudipatil2001@gmail.com>
2026-02-02 11:43:29 +0530

    UC2: Add Inches equality with JUnit tests

---
+++ src/main/java/com/qma/quantitymeasurement/Inches.java
@@ -0,0 +1,23 @@
+package com.qma.quantitymeasurement;
+
+public class Inches {
+    private final double value;
+
+    public Inches(double value) {
+        this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || this.getClass() != obj.getClass()) return false;
+
+        Inches other = (Inches) obj;
+        return Double.compare(this.value, other.value) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Double.hashCode(value);
+    }
+}
